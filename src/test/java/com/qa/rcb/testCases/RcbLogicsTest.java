0739d638677f4e2af496dc22bcc839b08f9a3dd2
irinrosejoseph <102680688+irinrosejoseph@users.noreply.github.com>
2022-09-23 21:44:41 +0400

    added the test methods to test the logics of foreign players count and wicket keepers count.

--- src/test/java/com/qa/rcb/testCases/RcbLogicsTest.java
+++ src/test/java/com/qa/rcb/testCases/RcbLogicsTest.java
@@ -1,5 +1,43 @@
 package com.qa.rcb.testCases;
 
-public class RcbLogicsTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.qa.rcb.base.RcbBase;
+import com.qa.rcb.logics.RcbLogics;
+
+public class RcbLogicsTest extends RcbBase{
+	
+	/*call the super constructor as properties file needed to 
+	 * be loaded to read property values
+	 * 
+	 */
+	public RcbLogicsTest()
+	{
+		super();
+	}
+	
+	//Test to assert the number of foreign players in the team
+	@Test(priority = 1)
+	public static void foreignPlayersLogicTest()
+	{
+		int fCount = RcbLogics.getForeignPlayersCount();
+		//get the master count of foreign players configured in the properties file.
+		int masterCount = Integer.parseInt(prop.getProperty("MAX_FOREIGN_CNT"));
+		
+		Assert.assertTrue(fCount<=masterCount, "The test failed as Total no of foreign players ("+fCount+") exceeded "+masterCount);
+	}
+	
+	//Test to assert atleast one wicket keeper in team
+	@Test(priority =2)
+	public static void wicketKeeperLogicTest()
+	{
+		int wCount = RcbLogics.getKeeperCount();
+		//get the master count of wicket keepers configured in the properties file.
+		int masterCount = Integer.parseInt(prop.getProperty("MIN_KEEPER_CNT"));
+		
+		Assert.assertTrue(wCount>=masterCount,"The test failed as atleast "+masterCount+" wicket keeper should be in the team. Actual number of wicket keepers is "+wCount);
+		
+	}
 
 }
